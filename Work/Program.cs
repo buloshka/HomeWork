95a87bc6b9946655baea50fe792bb704d7fe8823
buloshka <79216709+buloshka@users.noreply.github.com>
2023-06-02 23:52:32 +0300

    Third

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,50 +1,277 @@
 ﻿namespace HomeWork;
 
-public static class First
+public static class Third
 {
-    public static void Main()
+    static void Main()
     {
-        // Task 1
-        Console.WriteLine("Задание 1.");
-        for (int i = 1; i < 101; i++)
+        First();
+        Second();
+        Additionaly();
+    }
+
+    static void First()
+    {
+        string[,] table = {
+            { "Ученик", "Информатика(1)", "Разработка игр(2)", "Основы алгоритмизации(3)" },
+            { "1", "", "", "" },
+            { "2", "", "", "" },
+            { "3", "", "", "" },
+            { "4", "", "", "" },
+            { "5", "", "", "" }
+        };
+
+        Console.WriteLine("Задание 1");
+
+        DisplayTable(table);
+
+        bool exit = false;
+
+        while (!exit)
         {
-            if (i % 2 != 0) Console.Write(i + " ");
+            Console.Write("Хотите заполнить таблицу? (y/n): ");
+            string input = Console.ReadLine();
+
+            if (input == "y")
+            {
+                Console.Clear();
+                Console.WriteLine("Задание 1");
+                DisplayTable(table);
+
+                for (int studentIndex = 1; studentIndex < table.GetLength(0); studentIndex++)
+                {
+                    for (int subjectIndex = 1; subjectIndex < table.GetLength(1); subjectIndex++)
+                    {
+                        Console.Write($"Введите оценку {studentIndex} ученика {subjectIndex} предмета: ");
+                        string gradeString = Console.ReadLine();
+                        int grade;
+
+                        if (int.TryParse(gradeString, out grade) && grade > 1 && grade < 6)
+                        {
+                            table[studentIndex, subjectIndex] = grade.ToString();
+                            Console.Clear();
+                            DisplayTable(table);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный ввод.");
+                            subjectIndex--;
+                        }
+                    }
+                }
+
+                break;
+            }
+            else
+            {
+                exit = true;
+            }
         }
 
+        Console.WriteLine("Изменение таблицы");
 
-        // Task 2
-        Console.WriteLine("\n\nЗадание 2");
-        int firstNum = 0;
-        int secondNum = 1;
-
-        for (int i = 0; i < 10; i++)
+        while (!exit)
         {
-            Console.Write(firstNum + " ");
+            Console.Write("Введите номер ученика (1-5): ");
+            string student = Console.ReadLine();
+            int studentIndex;
 
-            firstNum = firstNum + secondNum;
-            secondNum = firstNum - secondNum;
+            if (int.TryParse(student, out studentIndex) && studentIndex >= 1 && studentIndex <= 5)
+            {
+                Console.Write("Введите номер предмета (1-3): ");
+                string subject = Console.ReadLine();
+                int subjectIndex;
+
+                if (int.TryParse(subject, out subjectIndex) && subjectIndex >= 1 && subjectIndex <= 3)
+                {
+                    Console.Write("Введите новую оценку(2, 3, 4, 5): ");
+                    string gradeString = Console.ReadLine();
+                    int grade;
+
+                    if (int.TryParse(gradeString, out grade) && grade > 1 && grade < 6)
+                    {
+                        table[studentIndex, subjectIndex] = grade.ToString();
+                        Console.Clear();
+                        DisplayTable(table);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Введите номер предмета от 1 до 3.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Введите номер ученика от 1 до 5.");
+            }
+
+            Console.WriteLine("Хотите внести еще изменения? (y/n): ");
+            string choice = Console.ReadLine();
+
+            if (choice.ToLower() != "y")
+            {
+                exit = true;
+                Console.Clear();
+            }
         }
 
+        Console.Clear();
+    }
 
-        // Task 3
-        Console.WriteLine("\n\nЗадание 3");
+    static void DisplayTable(string[,] table)
+    {
+        int rowCount = table.GetLength(0);
+        int columnCount = table.GetLength(1);
 
-        int start = 10;
-        double percent = 0.1;
-        int finish = 100;
+        for (int row = 0; row < rowCount; row++)
+        {
+            for (int col = 0; col < columnCount; col++)
+            {
+                Console.Write($"{table[row, col],-20} | ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
 
-        double days = Math.Round(Math.Log(finish / start, (1 + percent) * 10 / start), 5);
-        int seconds = (int)(days * 24 * 60 * 60);
-        int totalD = seconds / (24 * 60 * 60);
-        int totalH = (seconds / (60 * 60)) % 24;
-        int totalM = (seconds / 60) % 60;
-        int totalS = seconds % 60;
+    static void Second()
+    {
+        int[] array = GenerateRandomArray(10); // Создание массива из 10 случайных целых чисел
 
+        Console.WriteLine("Задание 2");
 
-        Console.WriteLine($"Лыжнику потребуется {totalD} дня " +
-                          $"{totalH} часа {totalM} минут {totalS} секунд, " +
-                          $"чтобы превысить 100 км");
+        Console.WriteLine("1) Все элементы массива:");
+        PrintArray(array);
 
+        Console.WriteLine("\n\n2) Элементы массива в обратном порядке:");
+        PrintReverseArray(array);
+
+        Console.WriteLine("\n\n3) Элементы массива через 1:");
+        PrintEvenIndex(array);
+
+        Console.WriteLine("\n\n4) Элементы массива до элемента -1:");
+        PrintArrayUntilMinusOne(array);
+
+        Console.WriteLine("\n\nНажмите любую клавишу, что перейти к след. заданию");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+    static int[] GenerateRandomArray(int length)
+    {
+        int[] array = new int[length];
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.Next(-100, 100); // Генерация случайного целого числа от 0 до 99
+        }
+
+        return array;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+    }
+
+    static void PrintReverseArray(int[] array)
+    {
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+    static void PrintEvenIndex(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+
+    static void PrintArrayUntilMinusOne(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == -1)
+            {
+                break;
+            }
+
+            Console.Write(array[i] + " ");
+        }
+    }
+
+    static void Additionaly()
+    {
+        int[,] temp =
+        {
+            { -8, -14, -19, -18 },
+            { 25, 28, 26, 20 },
+            { 11, 18, 20, 25 }
+        };
+
+        Console.WriteLine("Задание 3");
+
+        Console.WriteLine("1) Температура на 2 метеостанции за 4 день: " + temp[1, 3]);
+        Console.WriteLine("   Температура на 3 метеостанции за 1 день: " + temp[2, 0]);
+
+        Console.WriteLine("\n2) Значения температур всех метеостанций за 2 день:");
+        for (int i = 0; i < temp.GetLength(0); i++)
+        {
+            Console.WriteLine("   Метеостанция " + (i + 1) + ": " + temp[i, 1]);
+        }
+
+        Console.WriteLine("\n3) Значения температур всех метеостанций за все дни:");
+        Console.WriteLine("   Метеостанция | День 1 | День 2 | День 3 | День 4");
+        Console.WriteLine("   --------------------------------------------");
+        for (int i = 0; i < temp.GetLength(0); i++)
+        {
+            Console.Write("   Метеостанция " + (i + 1) + ": ");
+            for (int j = 0; j < temp.GetLength(1); j++)
+            {
+                Console.Write(temp[i, j] + "     ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.Write("\n4) Средняя температура на 3 метеостанции: ");
+        int totalTemperature = 0;
+        int count = 0;
+
+        for (; count < temp.GetLength(1); count++)
+        {
+            totalTemperature += temp[2, count];
+        }
+
+        double averageTemperature = (double)totalTemperature / count;
+
+        Console.WriteLine(averageTemperature);
+
+        Console.WriteLine("\n5) Метеостанции и дни с температурой в диапазоне 24-26 градусов:");
+        for (int i = 0; i < temp.GetLength(0); i++)
+        {
+            for (int j = 0; j < temp.GetLength(1); j++)
+            {
+                if (temp[i, j] >= 24 && temp[i, j] <= 26)
+                {
+                    Console.WriteLine("   Метеостанция " + (i + 1) + ", День " + (j + 1));
+                }
+            }
+        }
+
+        Console.WriteLine("Для завершения нажмите любую клавишу");
         Console.ReadKey();
     }
 }
